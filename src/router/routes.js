aa19c6abd50b1b7fdaf08b80e38c7fad6c4916de
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2020-02-20 00:36:10 -0600

    setup and wireframe

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,15 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  }
+  },
+  {
+    path: '/neu',
+    name: 'neu-cards',
+    component: () => import(/* webpackChunkName: "admin" */ '@/views/NeuCards.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
 
 ]
 
---
+++ src/views/NeuCards.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="neu-container">
+    <div class="neu-card neu-card-wrapper">
+      <div class="neu-card-column">
+        <div class="neu-card-container">
+
+        </div>
+        <div class="neu-card-container">
+          <div class="inner-card">
+            <div class="inner-card-card"></div>
+            <ul class="buttons">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+          </div>
+          <div class="balance-container"></div>
+          <div class="slider"></div>
+          <div class="credit-container"></div>
+        </div>
+      </div>
+      <div class="neu-card-column">
+        <div class="neu-card-container">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.neu-container {
+  // background-image: url('src/assets/neu-images/neumorphism-cards.png');
+}
+</style>
